3beed20b761cec5c06163ee934400e500c969c41
Anik7303 <anik7303@gmail.com>
2021-02-03 14:29:10 +0600

    added postComment & some validation

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,56 @@
 const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.postComment = functions.https.onCall((data, context) => {
+    checkAuthentication(context);
+    dataValidator(data, {
+        bookId: 'string',
+        comment: 'string',
+    });
+
+    const { comment, bookId } = data;
+    const db = admin.firestore();
+    const bookRef = db.collection('books').doc(bookId);
+
+    return db
+        .collection('profiles')
+        .where('userId', '==', context.auth.uid)
+        .limit(1)
+        .get()
+        .then((snapshot) =>
+            db.collection('comments').add({
+                content: comment,
+                user: snapshot.docs[0].id,
+                book: bookRef,
+                created: new Date(),
+            })
+        );
+});
+
+function checkAuthentication(context) {
+    if (!context.auth) {
+        throw new functions.https.HttpsError(
+            'unauthenticated',
+            'You must be signed in to use this feature'
+        );
+    }
+}
+
+function dataValidator(data, validKeys) {
+    if (Object.keys(data).length !== Object.keys(validKeys)) {
+        throw new functions.https.HttpsError(
+            'invalid-argument',
+            'Data object contains invalid number of properties'
+        );
+    }
+    for (let key in data) {
+        if (!validKeys[key] || typeof data[key] !== validKeys[key]) {
+            throw new functions.https.HttpsError(
+                'invalid-argument',
+                'Data object contains invalid properties'
+            );
+        }
+    }
+}
