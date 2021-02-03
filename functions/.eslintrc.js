83af39fd91383ab00dffd48a15ae06698fb3ca30
Anik7303 <anik7303@gmail.com>
2021-02-03 14:27:14 +0600

    initial commit

---
+++ functions/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'google'],
+    rules: {
+        quotes: ['error', 'single'],
+        indent: ['error', 4],
+        objectCurlySpacing: false,
+        requireJSDoc: false,
+    },
+};
