6bf1905ada31c8c03f8a6a792388d486b1b9ed9e
Anik7303 <anik7303@gmail.com>
2021-02-05 00:35:11 +0600

    added admin rule to  createProfile, and added addAuther & createBook functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project [Gatsby Book Club][gatsby-book-club] - Firebase Functions Portion
+
+[gatsby-book-club]: https://github.com/Anik7303/gatsby-book-club.git
--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,19 @@
 const functions = require('firebase-functions');
+const mimeTypes = require('mime-types');
 
 const admin = require('firebase-admin');
 admin.initializeApp();
+const DB = admin.firestore();
+const AUTH = admin.auth();
+const STORAGE = admin.storage();
 
 exports.createProfile = functions.https.onCall(async (data, context) => {
     checkAuthentication(context);
     dataValidator(data, { username: 'string' });
 
     const { username } = data;
-    const db = admin.firestore();
 
-    const userProfile = await db
-        .collection('profiles')
+    const userProfile = await DB.collection('profiles')
         .where('userId', '==', context.auth.uid)
         .limit(1)
         .get();
@@ -22,7 +24,7 @@ exports.createProfile = functions.https.onCall(async (data, context) => {
         );
     }
 
-    const profile = await db.collection('profiles').doc(username).get();
+    const profile = await DB.collection('profiles').doc(username).get();
     if (profile.exists) {
         throw new functions.https.HttpsError(
             'already-exists',
@@ -30,8 +32,12 @@ exports.createProfile = functions.https.onCall(async (data, context) => {
         );
     }
 
-    return db
-        .collection('profiles')
+    const user = await AUTH.getUser(context.auth.uid);
+    if (user.email === functions.config().accounts.admin) {
+        await AUTH.setCustomUserClaims(context.auth.uid, { admin: true });
+    }
+
+    return DB.collection('profiles')
         .doc(username)
         .set({ userId: context.auth.uid });
 });
@@ -44,16 +50,14 @@ exports.postComment = functions.https.onCall((data, context) => {
     });
 
     const { comment, bookId } = data;
-    const db = admin.firestore();
-    const bookRef = db.collection('books').doc(bookId);
+    const bookRef = DB.collection('books').doc(bookId);
 
-    return db
-        .collection('profiles')
+    return DB.collection('profiles')
         .where('userId', '==', context.auth.uid)
         .limit(1)
         .get()
         .then((snapshot) =>
-            db.collection('comments').add({
+            DB.collection('comments').add({
                 content: comment,
                 user: snapshot.docs[0].id,
                 book: bookRef,
@@ -62,11 +66,74 @@ exports.postComment = functions.https.onCall((data, context) => {
         );
 });
 
-function checkAuthentication(context) {
+exports.addAuthor = functions.https.onCall(async (data, context) => {
+    checkAuthentication(context, true);
+    dataValidator(data, { name: 'string' });
+
+    const exists = await DB.collection('authors')
+        .where('name', '==', data.name)
+        .get();
+    if (!exists.empty) {
+        throw new functions.https.HttpsError(
+            'already-exists',
+            'This author already exists'
+        );
+    }
+
+    return DB.collection('authors').add({ name: data.name });
+});
+
+exports.createBook = functions.https.onCall(async (data, context) => {
+    checkAuthentication(context, true);
+    dataValidator(data, {
+        title: 'string',
+        summary: 'string',
+        authorId: 'string',
+        bookCover: 'string',
+    });
+
+    const { title, summary, bookCover, authorId } = data;
+
+    const mimeType = bookCover.match(
+        /data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+).*,.*/
+    )[1];
+    const base64EncodedImageString = bookCover.replace(
+        /^data:image\/\w+;base64,/,
+        ''
+    );
+    const imageBuffer = new Buffer.from(base64EncodedImageString, 'base64');
+
+    const filename = `bookCovers/${title}.${mimeTypes.detectExtension(
+        mimeType
+    )}`;
+    const file = STORAGE.bucket().file(filename);
+    await file.save(imageBuffer, { contentType: 'image/jpeg' });
+    const fileUrls = await file.getSignedUrl({
+        action: 'read',
+        expires: '03-09-2491',
+    });
+
+    const authorRef = DB.collection('authors').doc(authorId);
+    return DB.collection('books').add({
+        title,
+        summary,
+        imageUrl: fileUrls[0],
+        author: authorRef,
+    });
+});
+
+function checkAuthentication(context, admin = false) {
     if (!context.auth) {
         throw new functions.https.HttpsError(
             'unauthenticated',
             'You must be signed in to use this feature'
+        );
+    }
+
+    if (admin && !context.auth.token.admin) {
+        throw new functions.https.HttpsError(
+            'permission-denied',
+            'You must be an admin to use this feature'
         );
     }
 }
